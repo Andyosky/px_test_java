d23810d1593f5cfcee8a3dd052800db64dc2d577
Andyosky <66939492+Andyosky@users.noreply.github.com>
2025-11-26 14:19:53 +0800

    Add files via upload

---
+++ GenerateHtml.java
@@ -0,0 +1,18 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class GenerateHtml {
+
+    public static void main(String[] args) {
+        String htmlContent = "<html>\n<head>\n<title>Hello World</title>\n</head>\n<body>\n<h1>Hello World!</h1>\n</body>\n</html>";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("index.html"))) {
+            writer.write(htmlContent);
+            System.out.println("Successfully created index.html");
+        } catch (IOException e) {
+            System.err.println("An error occurred while writing the HTML file.");
+            e.printStackTrace();
+        }
+    }
+}
